23ded04b91af59424cd21beacc1782643776b087
angelmfb <amfernandezbanos.guadalupe@alumnado.fundacionloyola.net>
2022-04-25 09:35:25 +0200

    correccion de algunos errores

--- controller/controlador.php
+++ controller/controlador.php
@@ -7,10 +7,10 @@
             parent:: __construct();
         }
         function altaMinijuego(){
-            if($_POST['nombre']==''){    //Compruebo que el nombre no se queda en blanco
+            if(empty($_POST['nombre'])){    //Compruebo que el nombre no se queda en blanco
                 echo ('No se puede quedar el nombre del usuario en blanco'); 
             }else{
-                if($_POST['ruta']==''){   //Compruebo si la ruta se queda en blanco
+                if(empty($_POST['ruta'])){   //Compruebo si la ruta se queda en blanco
                     echo ('La ruta no se puede quedar en blanco');
                 }else{
                         $this->alta();
